bf155f3ec049e681dee3b42b144084b51413e886
SonnyRR <9307969+SonnyRR@users.noreply.github.com>
2021-10-10 16:20:28 +0300

    Added a script for configuring shell preferences.

---
+++ setup-shell.ps1
@@ -0,0 +1,32 @@
+if ((Read-Host "Include custom .gitconfig: (y/N)").ToLower() -eq 'y') {
+    Write-Host "Include custom .gitconfig"
+    & git config --global include.path $pwd\git\.gitconfig
+}
+
+if ((Read-Host "Customize Powershell: (y/N)").ToLower() -eq 'y') {
+    & choco install `
+        cascadia-code-nerd-font `
+        -y
+    
+    Write-Host "Powershell: get modules"
+    Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+    Install-Module `
+        -Name @('Terminal-Icons') `
+        -Repository PSGallery
+
+    Install-Module PSReadLine -RequiredVersion 2.1.0
+
+    Write-Host  "Windows Terminal: Updating PWSH profile"
+    $runCmd = ". `"$pwd\\pwsh\\run.ps1`"";
+    If((Select-String -Path $PROFILE -Pattern $runCmd -SimpleMatch).Length -eq 0)
+    {
+        Write-Host 'Adding bootstrap script to $PROFILE'
+        Add-Content -Path $PROFILE -Value $([Environment]::NewLine) -NoNewline
+        Add-Content -Path $PROFILE -Value $runCmd
+    }
+
+    # replace font in Windows Terminal
+    Write-Host "Windows Terminal: Setting Nerd Font"
+    Install-Module -Name MSTerminalSettings -AllowPrerelease
+    Get-MSTerminalProfile | Set-MSTerminalProfile -FontFace "CaskaydiaCove Nerd Font"
+}

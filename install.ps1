2d7143e61726c27a000b9bfb35eeed1f1d5c1630
SonnyRR <9307969+SonnyRR@users.noreply.github.com>
2021-10-10 15:59:06 +0300

    Added a powershell script for installing common windows tools.

---
+++ install.ps1
@@ -0,0 +1,49 @@
+Write-Host "Installing/Updating Choco"
+if ($null -eq (Get-Command -Name choco.exe -ErrorAction SilentlyContinue)) {
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+} else {
+    & choco update Chocolatey
+}
+
+Write-Host  "Intstalling minimal setup"
+& choco install `
+    7zip `
+    git `
+    oh-my-posh `
+    microsoft-windows-terminal `
+    notepadplusplus `
+    powertoys `
+    pwsh `
+    sysinternals `
+    -y
+
+
+# add tools
+if ((Read-Host "Install dev tools: (y/N)").ToLower() -eq 'y') {
+    & choco install `
+        azure-cli `
+        dotnet `
+        docker-desktop `
+        insomnia-rest-api-client `
+        linqpad `
+        nodejs-lts `
+        nswagstudio `
+        postman `
+        vscode `
+        -y
+}
+
+if ((Read-Host "Install paid tools: (y/N)").ToLower() -eq 'y') {
+    & choco install `
+        visualstudio2019enterprise `
+        -y
+}
+
+if ((Read-Host "Install other apps: (y/N)").ToLower() -eq 'y') {
+    & choco install `
+        sumatrapdf.install `
+        jetbrainstoolbox `
+        -y
+}

b77eae203563944918948a7f1783f5c35e313258
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2021-12-11 19:06:00 +0200

    Included wrongly excluded assembly folder.

---
+++ system-init/SystemInit.Core/Packages/ChocolateyPackages.cs
@@ -0,0 +1,27 @@
+﻿namespace SystemInit.Packages
+{
+    public static class ChocolateyPackages
+    {
+        public static string[] Packages => new[]
+        {
+            "7zip",
+            "git",
+            "oh-my-posh",
+            "microsoft-windows-terminal",
+            "notepadplusplus",
+            "powertoys",
+            "pwsh",
+            "sysinternals",
+            "sumatrapdf.install",
+            "jetbrainstoolbox",
+            "azure-cli",
+            "dotnet",
+            "docker-desktop",
+            "insomnia-rest-api-client",
+            "linqpad",
+            "postman",
+            "vscode",
+            "nvm"
+        };
+    }
+}
---
+++ system-init/SystemInit.Core/Packages/WingetPackages.cs
@@ -0,0 +1,20 @@
+﻿namespace SystemInit.Packages
+{
+    public static class WingetPackages
+    {
+        public static string[] Packages => new[]
+        {
+            "Microsoft.VisualStudioCode",
+            "Docker.DockerDesktop",
+            "JetBrains.Toolbox",
+            "Microsoft.PowerShell",
+            "Microsoft.WindowsTerminal",
+            "Notepad++.Notepad++",
+            "SumatraPDF.SumatraPDF",
+            "Insomnia.Insomnia",
+            "Git.Git",
+            "qBittorrent.qBittorrent",
+            "Microsoft.PowerToys"
+        };
+    }
+}

280444dff7430ba124ab6351718bffda0ba7b9ac
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2022-05-10 11:26:58 +0300

    Disabled prompt status for `poshgit`.

--- pwsh/vkp.ps1
+++ pwsh/vkp.ps1
@@ -1,2 +1,3 @@
 # oh-my-posh --init --shell pwsh --config ~\AppData\Local\Programs\oh-my-posh\themes\slim.omp.json | Invoke-Expression
 oh-my-posh --init --shell pwsh --config ~\Documents\PowerShell\vk-cfg.json | Invoke-Expression
+$GitPromptSettings.EnablePromptStatus = $false

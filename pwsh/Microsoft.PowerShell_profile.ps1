0e42df8d299118e5a774618482a05f3f42234673
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2022-05-25 21:28:37 +0300

    Updated PS7 profile script.

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,3 @@
-# oh-my-posh --init --shell pwsh --config ~\AppData\Local\Programs\oh-my-posh\themes\slim.omp.json | Invoke-Expression
 oh-my-posh --init --shell pwsh --config ~\Documents\PowerShell\vk-cfg.json | Invoke-Expression
+Import-Module posh-git
 $GitPromptSettings.EnablePromptStatus = $false
